9d39fc5ed0222592d1f6611dc96f14b60341cbe9
claudewick <72236428+claudewick@users.noreply.github.com>
2022-05-11 10:11:16 -0300

    implementamos o hello world multi linguas

--- hello.py
+++ hello.py
@@ -1,3 +1,37 @@
-#!/c/Users/claud/AppData/Local/Programs/Python/Python39/python (este é um SHEBANG)
-# Este programa imprime Hello World
-print('hello world')
+#!/c/Users/claud/AppData/Local/Programs/Python/Python39/python (este é um SHEBANG usado apenas em scripts para indicar qual a versão do Python que ele deve rodar)
+""" Hello World Multi Linguas.
+
+Dependendo da língua configurada no ambiente oprograma exibe a mensagem correspondente.
+
+Como usar:
+
+Tenha a variável LANG configurada ex:
+    
+    export LANG=pt_BR
+    
+Execução:
+
+    python3 hello.py
+    ou
+    ./hello.py
+"""
+__version__ = '0.0.1'
+__author__ = 'Claude Wickihalder'
+__license__ = 'Unlicense'
+
+import os
+
+current_language = os.getenv('LANG', 'en_US')[:5]
+
+msg = 'Hello, World!'
+
+if current_language == 'pt_BR':
+    msg = 'Olá, Mundo!'
+elif current_language == 'it_IT':
+    msg = 'Ciao, Mondo!'
+elif current_language == 'es_SP':
+    msg = 'Hola, Mundo!'
+elif current_language == 'fr_FR':
+    msg = 'Bonjour Monde'
+
+print(msg)
